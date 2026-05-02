5d2e8c8436c61aba1b6a2d16b22bf4ad76d701b3
rahulkilaparthi <rahulkilaparthi7@gmail.com>
2026-05-02 13:54:08 +0530

    fix api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API,
 });
 
 api.interceptors.request.use((config) => {
